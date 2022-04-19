5b6db3c03a3d670c7491ca6ca2bbaf4f0b820cb4
Jonathan BARANZINI <titanbar@hotmail.com>
2022-04-19 13:41:34 +0200

    Update

--- src/model/task_manager_test.go
+++ src/model/task_manager_test.go
@@ -24,35 +24,54 @@ func TestManager(t *testing.T) {
 	assert.Equal(t, 1, len(groups["Resize"]))
 }
 
-func TestRemove(t *testing.T) {
+func TestRemoveMissingElement(t *testing.T) {
 	manager := NewManager()
+	task1 := Print{Id: manager.NextId()}
+	task2 := Print{Id: manager.NextId()}
+
 	assert.Equal(t, 0, manager.Size())
+
+	manager.Add(task1)
+	assert.Equal(t, 1, manager.Size())
+
+	manager.Remove(task2)
+	assert.Equal(t, 1, manager.Size())
+}
+
+func TestSimpleRemove(t *testing.T) {
+	manager := NewManager()
 	task1 := Print{Id: manager.NextId()}
 	task2 := Print{Id: manager.NextId()}
 	task3 := Print{Id: manager.NextId()}
 
 	manager.Add(task1)
-	assert.Equal(t, 1, manager.Size())
-	manager.Remove(task2)
-	assert.Equal(t, 1, manager.Size())
-
 	manager.Add(task2)
 	manager.Add(task3)
+
 	assert.Equal(t, 3, manager.Size())
 
 	manager.Remove(task1)
 	assert.Equal(t, 2, manager.Size())
 	assert.Equal(t, task2.GetId(), manager.GetAll()[0].GetId())
+}
+
+func TestComplexRemove(t *testing.T) {
+	manager := NewManager()
+	task1 := Print{Id: manager.NextId()}
+	task2 := Print{Id: manager.NextId()}
+	task3 := Print{Id: manager.NextId()}
+	task4 := Print{Id: manager.NextId()}
 
 	manager.Add(task1)
-	assert.Equal(t, 3, manager.Size())
+	manager.Add(task2)
+	manager.Add(task3)
+	manager.Add(task4)
+
+	assert.Equal(t, 4, manager.Size())
 
 	manager.Remove(task1)
-	assert.Equal(t, 2, manager.Size())
-
-	manager.Add(task1)
 	manager.Remove(task3)
+	assert.Equal(t, 2, manager.Size())
 	assert.Equal(t, task2.GetId(), manager.GetAll()[0].GetId())
-	assert.Equal(t, task1.GetId(), manager.GetAll()[1].GetId())
-
+	assert.Equal(t, task4.GetId(), manager.GetAll()[1].GetId())
 }
