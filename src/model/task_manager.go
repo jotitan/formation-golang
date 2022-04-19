59a1138962f11fe1081128b632f25eb6c5e6b8dc
Jonathan BARANZINI <titanbar@hotmail.com>
2022-04-19 13:29:22 +0200

    Update

--- src/model/task_manager.go
+++ src/model/task_manager.go
@@ -7,15 +7,24 @@ func NewManager() *Manager {
 	return &Manager{}
 }
 
-func (m *Manager) Add(task Task)    {}
-func (m *Manager) Remove(task Task) {}
+func (m *Manager) Add(task Task) {
+	// Write code here
+}
+func (m *Manager) Remove(task Task) {
+	// Write code here
+}
 func (m *Manager) GetAll() []Task {
+	// Write code here
 	return nil
 }
 
-func (m *Manager) Size() int { return 0 }
+func (m *Manager) Size() int {
+	// Write code here
+	return 0
+}
 
 //GroupByType return a map where key is a string type and list contains of task of this type
 func (m *Manager) GroupByType() map[string][]Task {
+	// Write code here
 	return nil
 }
