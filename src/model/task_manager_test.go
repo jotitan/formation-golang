d003f6745118ba72300d713d3f6e71d05b0efd53
Jonathan BARANZINI <titanbar@hotmail.com>
2022-05-05 14:23:14 +0200

    update atelier2

--- src/model/task_manager_test.go
+++ src/model/task_manager_test.go
@@ -7,33 +7,33 @@ import (
 func TestManager(t *testing.T) {
 	manager := NewManager()
 	if nb := manager.Size(); nb != 0 {
-		t.Error("Must found 0 but found", nb)
+		t.Error("Must find 0 but found", nb)
 	}
 
 	manager.Add(Print{})
 	if nb := manager.Size(); nb != 1 {
-		t.Error("Must found 1 but found", nb)
+		t.Error("Must find 1 but found", nb)
 	}
 	manager.Add(Print{})
 	if nb := manager.Size(); nb != 2 {
-		t.Error("Must found 2 but found", nb)
+		t.Error("Must find 2 but found", nb)
 	}
 
 	groups := manager.GroupByType()
 	if nb := len(groups); nb != 1 {
-		t.Error("Must found 1 but found", nb)
+		t.Error("Must find 1 but found", nb)
 	}
 	if nb := len(groups["Print"]); nb != 2 {
-		t.Error("Must found 2 but found", nb)
+		t.Error("Must find 2 but found", nb)
 	}
 	manager.Add(Resize{})
 	if nb := manager.Size(); nb != 3 {
-		t.Error("Must found 3 but found", nb)
+		t.Error("Must find 3 but found", nb)
 	}
 
 	groups = manager.GroupByType()
 	if nb := len(groups); nb != 2 {
-		t.Error("Must found 2 but found", nb)
+		t.Error("Must find 2 but found", nb)
 	}
 }
 
