a77910a57fc89627189771d29ddee42fd21a859d
Jonathan BARANZINI <titanbar@hotmail.com>
2022-05-06 01:09:03 +0200

    Correct test

--- src/model/task_manager_test.go
+++ src/model/task_manager_test.go
@@ -5,26 +5,58 @@ import (
 	"testing"
 )
 
-func TestManager(t *testing.T) {
+func TestEmptyManager(t *testing.T) {
+	// GIVEN
 	manager := NewManager()
-	assert.Equal(t, 0, manager.Size(), "Must found 0")
+	// WHEN
+	size := manager.Size()
+	// THEN
+	assert.Equal(t, 0, size, "Must find 0")
+}
 
+func TestAddOneManager(t *testing.T) {
+	// GIVEN
+	manager := NewManager()
 	manager.Add(NewPrint("", 0))
-	assert.Equal(t, 1, manager.Size(), "Must found 1")
+	// WHEN
+	size := manager.Size()
+	// THEN
+	assert.Equal(t, 1, size, "Must find 1")
+}
 
+func TestAddTwoManager(t *testing.T) {
+	// GIVEN
+	manager := NewManager()
 	manager.Add(NewPrint("", 0))
-	assert.Equal(t, 2, manager.Size(), "Must found 2")
+	manager.Add(NewPrint("", 0))
+	// WHEN
+	size := manager.Size()
+	// THEN
+	assert.Equal(t, 2, size, "Must find 2")
+}
 
+func TestGroupManager(t *testing.T) {
+	// GIVEN
+	manager := NewManager()
+	manager.Add(NewPrint("", 0))
+	manager.Add(NewPrint("", 0))
+	// WHEN
 	groups := manager.GroupByType()
-	assert.Equal(t, 1, len(groups), "Must found 1")
+	// THEN
+	assert.Equal(t, 1, len(groups), "Must find 1")
+	assert.Equal(t, 2, len(groups["print"]), "Must find 2")
+}
 
-	assert.Equal(t, 2, len(groups["print"]), "Must found 2")
-
+func TestGroupTwoManager(t *testing.T) {
+	// GIVEN
+	manager := NewManager()
+	manager.Add(NewPrint("", 0))
+	manager.Add(NewPrint("", 0))
 	manager.Add(NewResize("", "", 0, 0, 0))
-	assert.Equal(t, 3, manager.Size(), "Must found 3")
-
-	groups = manager.GroupByType()
-	assert.Equal(t, 2, len(groups), "Must found 2")
+	// WHEN
+	groups := manager.GroupByType()
+	// THEN
+	assert.Equal(t, 2, len(groups), "Must find 2")
 }
 
 func TestRemoveMissingElement(t *testing.T) {
@@ -33,21 +65,22 @@ func TestRemoveMissingElement(t *testing.T) {
 	task2 := NewPrint("", manager.NextId())
 
 	if nb := manager.Size(); nb != 0 {
-		t.Error("Must find 0, but found", nb)
+		t.Error("Must find 0, but find", nb)
 	}
 
 	manager.Add(task1)
 	if nb := manager.Size(); nb != 1 {
-		t.Error("Must find 1, but found", nb)
+		t.Error("Must find 1, but find", nb)
 	}
 
 	manager.Remove(task2)
 	if nb := manager.Size(); nb != 1 {
-		t.Error("Must find 1, but found", nb)
+		t.Error("Must find 1, but find", nb)
 	}
 }
 
 func TestSimpleRemove(t *testing.T) {
+	// GIVEN
 	manager := NewManager()
 	task1 := NewPrint("", manager.NextId())
 	task2 := NewPrint("", manager.NextId())
@@ -58,12 +91,12 @@ func TestSimpleRemove(t *testing.T) {
 	manager.Add(task3)
 
 	if nb := manager.Size(); nb != 3 {
-		t.Error("Must find 3, but found", nb)
+		t.Error("Must find 3, but find", nb)
 	}
 
 	manager.Remove(task1)
 	if nb := manager.Size(); nb != 2 {
-		t.Error("Must find 2, but found", nb)
+		t.Error("Must find 2, but find", nb)
 	}
 	if manager.GetAll()[0].Id() != task2.Id() {
 		t.Error("Must find", task2.Id())
@@ -83,13 +116,13 @@ func TestComplexRemove(t *testing.T) {
 	manager.Add(task4)
 
 	if nb := manager.Size(); nb != 4 {
-		t.Error("Must find 4, but found", nb)
+		t.Error("Must find 4, but find", nb)
 	}
 
 	manager.Remove(task1)
 	manager.Remove(task3)
 	if nb := manager.Size(); nb != 2 {
-		t.Error("Must find 2, but found", nb)
+		t.Error("Must find 2, but find", nb)
 	}
 	if manager.GetAll()[0].Id() != task2.Id() {
 		t.Error("Must find", task2.Id())
